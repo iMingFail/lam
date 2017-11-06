0bb342cfa3cb117c1f062e6c3a8f1ff86b535e99
wzm <178738517@qq.com>
2017-11-06 21:23:01 +0800

    查询优化

--- app/Http/Controllers/Admin/LunxunController.php
+++ app/Http/Controllers/Admin/LunxunController.php
@@ -192,7 +192,9 @@ class LunxunController extends Controller
 			event(new \App\Events\userActionEvent('\App\Models\Admin\Lunxun', 0, 1, '批量执行轮询' ));
 			return redirect('/admin/lunxun');
 		}
-		return redirect('/admin/lunxun/show/'.$i);
+		echo "正在轮询·······";
+		echo "<script>window.location='/admin/lunxun/show/".$i."';</script>";
+		//return redirect('/admin/lunxun/show/'.$i);
     }
 
     /**
